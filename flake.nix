ca6f22a88a6fba5a656665ae15915f8cde851e36
SkohTV <qlpth+github@proton.me>
2024-12-12 08:36:45 +0100

    added bases

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{ outputs = { nixpkgs, ... }:
+
+let
+  allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+  forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+    pkgs = import nixpkgs { inherit system; };
+  });
+
+in {
+  devShells = forAllSystems ({ pkgs }: {
+    default = pkgs.mkShell {
+      packages = with pkgs; [
+        # non_python_package
+
+        (python313.withPackages (ps: with ps; [
+          virtualenv
+          pip
+
+          requests
+        ]))
+      ];
+    };
+
+  });
+};
+}
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.api.test import query
 
 
-
+query()
