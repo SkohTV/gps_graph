94151928314fef831fdfee0a0e1b44937436b89f
SkohTV <qlpth+github@proton.me>
2024-12-13 11:39:50 +0100

    small main update

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-import json
+from src.args import setup_parser
+from src.logging import logger
+from src.core import verify_integrity_cache
+from src.logging import setup_logging
+from src.api_requests import update_cache_map
+from src.args import arguments
 
-from src.api.cache import update_cache_map
 
+
+def main():
+  verify_integrity_cache()
+  setup_parser()
+  setup_logging()
+  update_cache_map()
 
 
 if __name__ == "__main__":
-  update_cache_map(force=True)
+  main()
 
+
+  logger.debug("debug message", extra={"x": "hello"})
+  logger.info("info message")
+  logger.warning("warning message")
+  logger.error("error message")
+  logger.critical("critical message")
